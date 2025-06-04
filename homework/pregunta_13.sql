2eb48952798ed6ae8f654bbb6b660c0daeff4e93
Christopher Obando <cobandor@unal.edu.co>
2025-06-04 14:52:35 -0500

    SQL con SQlite

--- homework/pregunta_13.sql
+++ homework/pregunta_13.sql
@@ -42,4 +42,15 @@
 --  4  E  474.825000
 -- 
 --  >>> Escriba su codigo a partir de este punto <<<
---  
+SELECT 
+  K0, 
+  ROUND(AVG(c12), 2) AS "avg(c12)"
+FROM tbl1
+WHERE c13 > 400
+GROUP BY K0
+ORDER BY K0;
+
+
+
+
+
